76e4dbb35f6f6385ecb45d5f6d49ac93121c3616
Derick Ostrenko <dostrenko@gmail.com>
2017-05-03 22:44:08 -0500

    up a dir

--- scripts/install.js
+++ scripts/install.js
@@ -6,20 +6,20 @@ var dirp5 = "web";
 var dirarduino = "arduino";
 var dirnode = "node";
 
-if (!fs.existsSync("../"+dirp5)){
-    fs.mkdirSync("../"+dirp5);
+if (!fs.existsSync("../../"+dirp5)){
+    fs.mkdirSync("../../"+dirp5);
 }
 
-if (!fs.existsSync("../"+dirarduino)){
-    fs.mkdirSync("../"+dirarduino);
+if (!fs.existsSync("../../"+dirarduino)){
+    fs.mkdirSync("../../"+dirarduino);
 }
 
-fs.createReadStream(dirarduino+"/"+dirarduino+".ino").pipe(fs.createWriteStream("../"+dirarduino+"/"+dirarduino+".ino"));
-fs.createReadStream(dirp5+"/sketch.js").pipe(fs.createWriteStream("../"+dirp5+"/sketch.js"));
-fs.createReadStream(dirp5+"/index.html").pipe(fs.createWriteStream("../"+dirp5+"/index.html"));
+fs.createReadStream(dirarduino+"/"+dirarduino+".ino").pipe(fs.createWriteStream("../../"+dirarduino+"/"+dirarduino+".ino"));
+fs.createReadStream(dirp5+"/sketch.js").pipe(fs.createWriteStream("../../"+dirp5+"/sketch.js"));
+fs.createReadStream(dirp5+"/index.html").pipe(fs.createWriteStream("../../"+dirp5+"/index.html"));
 
 http.get("https://raw.githubusercontent.com/vanevery/p5.serialport/master/lib/p5.serialport.js", function(response) {
-  response.pipe(fs.createWriteStream("../"+dirp5+"/p5.serialport.js"));
+  response.pipe(fs.createWriteStream("../../"+dirp5+"/p5.serialport.js"));
 });
 
 var options = {
@@ -34,7 +34,7 @@ function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
     var p5minjsurl = info.assets[3].browser_download_url;
-    request(p5minjsurl).pipe(fs.createWriteStream("../"+dirp5+"/p5.min.js"))
+    request(p5minjsurl).pipe(fs.createWriteStream("../../"+dirp5+"/p5.min.js"))
   }
 }
 
