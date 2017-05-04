cf8c57c4a176aaedd3e330c6c762aa41de21bf6b
Derick Ostrenko <dostrenko@gmail.com>
2017-05-03 22:48:12 -0500

    added example code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,27 @@
+int ledPin = 13;            // the pin that the LED is attached to
+int incomingByte;           // a variable to read incoming serial data into
+
 void setup() {
-
+  Serial.begin(9600);       // initialize serial communications
+  pinMode(ledPin, OUTPUT);  // initialize the LED pin as an output
 }
-
+ 
 void loop() {
+  int potentiometer = analogRead(A0);					// read the input pin
+  int mappedPot = map(potentiometer, 0, 1023, 0, 255);	// remap the pot value to fit in 1 byte
+  // Serial.println(potentiometer);						// Print out a string to the serial port. Use with serial.readStringUntil("\r\n") in p5.
+  Serial.write(mappedPot);								// Print out a byte to the serial port. Use with serial.read in p5.
+
+  if (Serial.available() > 0) {   // see if there's incoming serial data
+    incomingByte = Serial.read(); // read it
+    
+    if (incomingByte == 'H') {    // if it's a capital H (ASCII 72),
+      digitalWrite(ledPin, HIGH); // turn on the LED
+    }
+
+    if (incomingByte == 'L') {    // if it's an L (ASCII 76)
+      digitalWrite(ledPin, LOW);  // turn off the LED
+    }
+  }
 
 }
--- web/sketch.js
+++ web/sketch.js
@@ -1,7 +1,40 @@
-function setup() {
+// Serial port global variables
 
+var serial;                               // Variable to hold instance of serialport library
+var portName = '/dev/cu.usbmodem1411';    // Fill in  serial port name here
+
+// Serial data global variables
+
+var inData;                               // For incoming serial data
+var outData;                              // For outgoing serial data
+
+// Setup the serial port for communication
+
+function setup() {
+  serial = new p5.SerialPort();           // Make new instance of serialport library
+  serial.on('data', serialEvent);         // Callback when new data arrives
+  serial.on('error', serialError);        // Callback for errors
+  serial.open(portName);                  // Open serial port
 }
 
-function draw() {
+// Error Logging
 
+function serialError(err) {
+  console.log('Something went wrong with the serial port:', err);
+}
+
+// Read data from the serial port
+
+function serialEvent() {
+  inData = serial.read();                   // Read *bytes* from serial port. Used with serial.write from the Arduino.
+  // inData = serial.readStringUntil("\r\n");  // Read *strings* from the serial port. Used with serial.println from the Arduino.
+  console.log(inData);                      // Log incoming data
+}
+
+// Write data to the serial port
+
+function keyPressed() {
+  outData = key; 
+  console.log("Writing to Serial Port", outData);
+  serial.write(outData);                    // Send data out the serial port
 }
